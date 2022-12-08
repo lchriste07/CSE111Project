4ba36b195f63be292193e163686d0d88f2ad1845
Brodey-Nance <brodeynance@gmail.com>
2022-12-07 17:10:14 -0800

    Updated with more features added and got some stuff working

--- expressjs-multi-page-site-demo-main/server.js
+++ expressjs-multi-page-site-demo-main/server.js
@@ -47,55 +47,158 @@ const port = "8000";
 });
  
  // Get list of all matches from the rated section
- app.get("/matchlist", (req, res) => {
+ app.get("/ratedmatchlist", (req, res) => {
   let sql = `SELECT * FROM rated_matches limit 200`;
   db.all(sql, [], (err, rows) => {
     if (err) {
       throw err;
     }
-    return res.render("matchlist" , { title: 'index', index: rows})
+    return res.render("ratedmatchlist" , { title: 'index', index: rows})
+  });
+ });
+ 
+// gets the unrated matchlist
+ app.get("/unratedmatchlist", (req, res) => {
+  let sql = `SELECT * FROM unrated_matches limit 200`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    return res.render("unratedmatchlist" , { title: 'index', index: rows})
   });
  });
 
 
 
-
-
- 
+ // Renders the page to take the inputs
  app.get("/account", (req, res) => {
    res.render("account");
  });
 
  //Once we get the ID it will render the section with 
- app.post("/account", (req, res) => {
+ app.post("/useraccount", (req, res) => {
   // Query for the account and get the information from the account and fill out a table. 
     const {emailAddress} = req.body;
     var sql="SELECT * FROM account WHERE acc_accountid = ? limit 1";
     db.all(sql, [emailAddress] ,(err, data) =>{
       if (err) throw err;
-      if (err){
+      
+      if(data[0] === undefined){
         alert("Invalid Login Credentials");
-         return res.render("account");
-      }
-      if(data[0].acc_accountid === emailAddress){
-         res.render("editInfo", { title: 'index', index: data});
+      }else{
+        return res.render("editInfo", { title: 'index', index: data});
       }
       
     });
 
-
-  res.render("editinfo");
 });
 
-app.post("/editinfo", (req, res) => {
-  // Query for the account and get the information from the account and fill out a table. 
+
+app.get("/matchhistory", (req, res) => {
+  const ID = req.query.ID;
+  var sql="SELECT * FROM rated_matches WHERE rm_white_id = ? or rm_black_id = ?";
+  db.all(sql, [ID,ID] ,(err, data) =>{
+    if (err) throw err;
+    
+    if(data[0] === undefined){
+      alert("Invalid ID Credentials");
+    }else{
+      return res.render("matchhistory", { title: 'index', index: data});
+    }
+    
+  });
+
+ });
 
 
-
-  res.render("index");
-  alert("Thank for editing your info!")
+app.get("/inputUser", (req, res) => {
+  res.render("input");
 });
- 
+
+app.post("/inputUser", (req, res) => {
+  const { ID, DOB, emailAddress } = req.body;
+
+  // Construct a SQL query to check whether the email address already exists in the table
+  var sql = `SELECT * FROM account WHERE acc_email = ?`;
+
+  // Use the db.get method to run the query and retrieve the data from the table
+  db.get(sql, [emailAddress], function (err, data) {
+    if (err) {
+      // If an error occurs, log it and send an error response to the client
+      console.error(err);
+      res.send({ error: "An error occurred while accessing the database" });
+      return;
+    }
+
+    if (data) {
+      // If the email address already exists in the table, send an error response to the client
+     alert("This email already exists in this table");
+    } else {
+      // Otherwise, insert a new row into the account table
+      db.run(
+        `INSERT INTO account VALUES(?,?,?,?,?)`,
+        [ID, DOB, emailAddress, 0, 0],
+        function (err) {
+          if (err) {
+            // If an error occurs, log it and send an error response to the client
+            console.error(err);
+            alert("An error occurred while adding the account");
+            return;
+          }
+
+          // Send a success response to the client
+          res.redirect("/");
+        }
+      );
+    }
+  });
+});
+
+
+app.get("/inputGame", (req, res) => {
+  res.render("inputmatches");
+});
+
+app.post("/inputGame", (req, res) => {
+  const { ID, DOB, emailAddress } = req.body;
+  console.log(ID);
+  console.log(DOB);
+  // Construct a SQL query to check whether the email address already exists in the table
+  var sql = `SELECT * FROM account WHERE acc_email = ?`;
+
+  // Use the db.get method to run the query and retrieve the data from the table
+  db.get(sql, [emailAddress], function (err, data) {
+    if (err) {
+      // If an error occurs, log it and send an error response to the client
+      console.error(err);
+      res.send({ error: "An error occurred while accessing the database" });
+      return;
+    }
+
+    if (data) {
+      // If the email address already exists in the table, send an error response to the client
+      res.send({ error: "This email already exists in this table" });
+    } else {
+      // Otherwise, insert a new row into the account table
+      db.run(
+        `INSERT INTO account VALUES(?,?,?,?,?)`,
+        [ID, DOB, emailAddress, 0, 0],
+        function (err) {
+          if (err) {
+            // If an error occurs, log it and send an error response to the client
+            console.error(err);
+            res.send({ error: "An error occurred while adding the account" });
+            return;
+          }
+
+          // Send a success response to the client
+          res.redirect("/");
+        }
+      );
+    }
+  });
+});
+
 
 app.get("/openings",function(req, res) {
   var sql='SELECT * FROM opening limit 200';
@@ -114,6 +217,8 @@ app.get("/openings",function(req, res) {
   res.render('leaderboard', { title: 'Leaderboard Ranking', index: data});
 });
  });
+
+ 
  
  
  // Import
